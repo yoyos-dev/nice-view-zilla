7b2975e6afe83fd0499588f239875717cb10b0ab
Johan Sandoval <jojo.han718@gmail.com>
2025-02-19 12:59:09 -0600

    Added Nicezilla

--- boards/shields/nice_view_custom/widgets/art.c
+++ boards/shields/nice_view_custom/widgets/art.c
@@ -234,13 +234,13 @@ const lv_img_dsc_t mountain = {
 
 const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_NICEZILLA uint8_t 
   nicezilla_map[] = {
-    #if CONFIG_NICE_VIEW_WIDGET_INVERTED
+  #if CONFIG_NICE_VIEW_WIDGET_INVERTED
         0xff, 0xff, 0xff, 0xff, /*Color of index 0*/
         0x00, 0x00, 0x00, 0xff, /*Color of index 1*/
-    #else
+  #else
         0x00, 0x00, 0x00, 0xff, /*Color of index 0*/
         0xff, 0xff, 0xff, 0xff, /*Color of index 1*/
-    #endif
+  #endif
 
   0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xf0,
   0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xf0,
@@ -313,11 +313,11 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_NICEZILLA
 };
   
 const lv_img_dsc_t nicezilla = {
-  .header.cf = LV_IMG_CF_ALPHA_1BIT,
+  .header.cf = LV_IMG_CF_INDEXED_1BIT,
   .header.always_zero = 0,
   .header.reserved = 0,
   .header.w = 140,
   .header.h = 68,
-  .data_size = 1224,
+  .data_size = 1232,
   .data = nicezilla_map,
 };
