746947c5c2f561e25f705e111348cd4ccd9dc4e0
Johan Sandoval <jojo.han718@gmail.com>
2025-02-20 18:26:09 -0600

    Another syntax fix

--- boards/shields/nice_view_custom/widgets/art.c
+++ boards/shields/nice_view_custom/widgets/art.c
@@ -312,7 +312,7 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_NICEZILLA
   0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xf0,
 };
   
-  nicezilla = {
+  const lv_img_dsc_t nicezilla = {
   .header.cf = LV_IMG_CF_INDEXED_1BIT,
   .header.always_zero = 0,
   .header.reserved = 0,
